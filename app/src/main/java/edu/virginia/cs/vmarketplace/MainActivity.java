8b3ec1c64acc1cf05e649be7d8303b01933ba920
Minghua Jiang <61221630@qq.com>
2017-11-21 12:45:50 -0500

    use java instead of kotlin

---
+++ app/src/main/java/edu/virginia/cs/vmarketplace/MainActivity.java
@@ -0,0 +1,17 @@
+package edu.virginia.cs.vmarketplace;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cutehuazai on 11/21/17.
+ */
+
+public class MainActivity extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.activity_main);
+    }
+}
