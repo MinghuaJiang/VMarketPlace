c331fcded4ffb95a156a2efe785f01c79c585492
Xinge Wen <xinge.wensxwj@gmail.com>
2017-12-04 20:33:32 -0500

    update userProfileDO

--- app/src/main/java/edu/virginia/cs/vmarketplace/view/fragments/HomePostAdapter.java
+++ app/src/main/java/edu/virginia/cs/vmarketplace/view/fragments/HomePostAdapter.java
@@ -1,6 +1,7 @@
 package edu.virginia.cs.vmarketplace.view.fragments;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -8,11 +9,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
 import edu.virginia.cs.vmarketplace.R;
 import edu.virginia.cs.vmarketplace.model.nosql.ProductItemsDO;
+import edu.virginia.cs.vmarketplace.model.nosql.UserProfileDO;
 
 /**
  * Created by VINCENTWEN on 12/4/17.
@@ -32,15 +35,39 @@ public class HomePostAdapter extends ArrayAdapter<ProductItemsDO> {
                     ,false);
         }
         ProductItemsDO productItemsDO = getItem(position);
-        UserDO userDO = getUser(productItemsDO.getUserId());
+        new UserProfileDOLoadAsyncTask().execute("");
 
-        ImageView userAvatar = listView.findViewById(R.id.home_post_avatar);
-        if(userDO.getAvatar() == null) {
-            userAvatar.setImageResource(R.drawable.user_24p);
-        } else {
-            Picasso.with(getContext()).load(userDO.getAvatar()).
-                    placeHolder(R.drawable.product_placeholder_96dp).into(userAvatar);
+
+
+    }
+
+    class UserProfileDOLoadAsyncTask extends AsyncTask<String, Void, UserProfileDO> {
+        private View rootView;
+
+        public UserProfileDOLoadAsyncTask(View view) {
+            this.rootView = view;
         }
 
+        @Override
+        protected UserProfileDO doInBackground(String... strings) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(UserProfileDO userProfileDO) {
+            ImageView userAvatar = rootView.findViewById(R.id.home_post_avatar);
+            if(userProfileDO.getAvatar() == null) {
+                userAvatar.setImageResource(R.drawable.user_24p);
+            } else {
+                Picasso.with(getContext()).load(userProfileDO.getAvatar()).
+                        placeholder(R.drawable.product_placeholder_96dp).into(userAvatar);
+            }
+
+            TextView userName = rootView.findViewById(R.id.home_post_user_name);
+            userName.setText(userProfileDO.getUserName());
+
+            TextView userDep = rootView.findViewById(R.id.home_post_user_dep);
+            userDep.setText(userProfileDO.);
+        }
     }
 }
